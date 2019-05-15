3eacf080c4465d2c939a7e92b86f6e6b32ed744a
Martin Nate <martin.nate.contact@gmail.com>
2019-05-15 13:21:46 +0700

    Initial Commit

---
+++ coffeeMachinery/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  coffeeMachinery
+//
+//  Created by Martin Nate on 15/05/19.
+//  Copyright © 2019 Martin Nate. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
